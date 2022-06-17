9684a52d2664c90f8d3861b92db2d815aea23792
Bloodstone2179 <87939217+Bloodstone2179@users.noreply.github.com>
2022-06-17 21:18:26 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
   description = 'a module for amazon tha gets the price and name of the product',   # Give a short description about your library
   author = 'tim harris',                   # Type in your name
   author_email = 'your.email@domain.com',      # Type in your E-Mail
-  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  url = 'https://github.com/Bloodstone2179/AmazonApi-Unoffical-',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/Bloodstone2179/AmazonApi-Unoffical-/archive/refs/tags/v_01.tar.gz',    # I explain this later on
   keywords = ['AMAZON', 'API', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[
           'requests',
